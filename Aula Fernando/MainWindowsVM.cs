9fe7a24150be0566ff9bfdd94d8ad046a0000e29
yangshindo <eliwood182@hotmail.com>
2022-12-26 14:56:53 -0300

    Adicionar arquivos de projeto.

---
+++ Aula Fernando/MainWindowsVM.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections.ObjectModel;
+using System.Windows.Input;
+using Npgsql;
+
+namespace Aula_Fernando
+{
+    public class MainWindowsVM
+    {
+        public ObservableCollection<User> UsersList { get; set; } //variável para ser linkada ao objeto
+
+        public ICommand Add { get; set; } //comando adicionar (ICommand interface, RelayCommand class)
+
+        public ICommand Remove { get; set; }
+
+        public ICommand Edit { get; set; }
+
+        public User SelectedUser { get; set; }
+
+        private static NpgsqlConnection GetConnection()
+        {
+            return new NpgsqlConnection(@"Server=localhost;Port=5432;User Id=postgres;Password=1234;Database=WPFDB");
+        }
+
+        private static void InsertRecord()
+        {
+            using(NpgsqlConnection con=GetConnection())
+            {
+                string query = @"insert into public.Users";
+            }
+        }
+
+        // Construtor
+        public MainWindowsVM()
+        {
+            UsersList = new ObservableCollection<User>()
+            {
+
+            };
+            IniciaComandos();
+        }
+
+
+        public void IniciaComandos()
+        {
+
+            Add = new RelayCommand((object _) =>
+            {
+                User userCadastro = new User(); //novo usuário em branco
+
+                CadastroUsuario tela = new CadastroUsuario();  //CadastroUsuario é uma tela mas também é uma classe
+
+                tela.DataContext = userCadastro; //define o context da tela para salvar os dados digitados no usuário
+
+       
+                bool? verifica = tela.ShowDialog();
+                if (verifica.HasValue && verifica.Value)
+                {
+                    UsersList.Add(userCadastro);
+                }
+
+                
+                using (NpgsqlConnection con = GetConnection())
+                {
+                    string query = @"insert into public.Users(Name,Email,Password)values("+userCadastro.Name+","+userCadastro.Email+","+userCadastro.Password+")";
+                    NpgsqlCommand cmd = new NpgsqlCommand(query, con);
+                    con.Open();
+                    int n = cmd.ExecuteNonQuery();
+                    if (n == 1)
+                    {
+                        Console.WriteLine("Record inserted");
+                    }
+
+                }
+                
+                
+
+            });
+
+
+            Edit = new RelayCommand((object _) =>
+            {
+                if (SelectedUser != null) {
+                    User usuario = SelectedUser.Clone();
+
+                    CadastroUsuario telaAtualizar = new CadastroUsuario();
+
+                    telaAtualizar.DataContext = usuario;
+
+                    bool? verifica = telaAtualizar.ShowDialog();
+
+                    if (verifica.HasValue && verifica.Value)
+                    {
+                        SelectedUser.Name = usuario.Name;
+                        SelectedUser.Email = usuario.Email;
+                        SelectedUser.Password = usuario.Password;
+                    }
+                }
+                
+            }
+            );
+
+            Remove = new RelayCommand((object _) => { UsersList.Remove(SelectedUser); }); // utiliza função remove no usuário selecionado (ItemSelected no ListView)
+
+
+        }
+
+
+    }
+}
---
+++ Aula Fernando/User.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aula_Fernando
+{
+    public class User : INotifyPropertyChanged
+    {
+
+        private string name;
+        private string email;
+        private string password;
+
+        public User()
+        {
+
+        }
+
+        public User(string name, string email, string password)
+        {
+            this.name = name;
+            this.email = email;
+            this.password = password;
+        }
+
+
+        // Definindo os métodos getters e setters de cada propriedade
+        public string Name
+        {
+            get { return name; }
+            set
+            {
+                if (name != value)
+                {
+                    name = value;
+                    RaisePropertyChanged("Name");
+                }
+            }
+        }
+
+        public string Email
+        {
+            get { return email; }
+            set
+            {
+                if (email != value)
+                {
+                    email = value;
+                    RaisePropertyChanged("Email");
+                }
+            }
+        }
+
+        public string Password
+        {
+            get { return password; }
+            set
+            {
+                if (password != value)
+                {
+                    password = value;
+                    RaisePropertyChanged("Password");
+                }
+            }
+        }
+
+        public User Clone()
+        {
+            return (User)this.MemberwiseClone();
+        }
+
+        //-- implementação da interface INotifyPropertyChanged
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private void RaisePropertyChanged(string prop)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(prop));
+        }
+
+    }
+}
